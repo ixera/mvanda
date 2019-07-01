16bfe9942dc5c0fa8f3cf0613fdd9dd33972a412
elyatai <ellieyatai@gmail.com>
2019-07-01 10:03:12 -0700

    add more backslashes
    hopefully don't summon ba'al

--- src/Parse.hs
+++ src/Parse.hs
@@ -4,6 +4,8 @@ import Text.Parsec hiding (parse)
 import qualified Text.Parsec as P
 import Data.Functor.Identity (Identity)
 import qualified Data.Map.Strict as M
+import Numeric (readHex)
+import Data.Char (chr)
 
 import Types
 import Instructions (instructions)
@@ -59,9 +61,21 @@ strEscape :: Parser Char
 strEscape = do
   char '\\'
   ch <- anyChar
-  return $ case ch of
-    'n'  -> '\n'
-    _    -> ch
+  case ch of
+    'x' -> hex 2
+    'u' -> hex 4
+    'U' -> hex 8
+    _   -> return $ case ch of
+      '0' -> '\NUL'
+      'e' -> '\ESC'
+      'n' -> '\n'
+      'r' -> '\r'
+      _   -> ch
+
+hex :: Int -> Parser Char
+hex n = do
+  x <- (fst . head . readHex) <$> count n hexDigit
+  return $ chr x
 
 instr :: String -> Parser Mvanda
 instr s = case M.lookup s instructions of
